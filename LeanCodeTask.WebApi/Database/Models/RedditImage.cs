74b746f756425f8a04805a65618ebcfd56d13e46
Jan Szablanowski <szablanowsj@gmail.com>
2022-08-10 23:12:45 +0200

    Move Models folder to Database

--- LeanCodeTask.WebApi/Database/Models/RedditImage.cs
+++ LeanCodeTask.WebApi/Database/Models/RedditImage.cs
@@ -1,7 +1,7 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace LeanCodeTask.WebApi.Models;
+namespace LeanCodeTask.WebApi.Database.Models;
 
 public class RedditImage
 {
