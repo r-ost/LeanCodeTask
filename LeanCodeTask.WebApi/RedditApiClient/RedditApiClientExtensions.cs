9d6f604f5c8784f44799625d992300636b3da944
Jan Szablanowski <szablanowsj@gmail.com>
2022-08-10 22:55:20 +0200

    Move RedditApiClient registration to RedditApiClientExtensions

---
+++ LeanCodeTask.WebApi/RedditApiClient/RedditApiClientExtensions.cs
@@ -0,0 +1,30 @@
+using IdentityModel.Client;
+using Refit;
+
+namespace LeanCodeTask.WebApi.RedditApiClient;
+
+public static class RedditApiClientExtensions
+{
+    public static IServiceCollection AddRedditApiClient(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAccessTokenManagement(options =>
+        {
+            options.Client.Clients.Add("reddit-api", new ClientCredentialsTokenRequest
+            {
+                RequestUri = new Uri(configuration["RedditApi:TokenEndpoint"]),
+                ClientId = configuration["RedditApi:ClientId"],
+                ClientSecret = configuration["RedditApi:ClientSecret"],
+                GrantType = "client_credentials",
+            });
+        });
+        
+        services.AddRefitClient<IRedditApiClient>()
+            .ConfigureHttpClient(client =>
+            {
+                client.BaseAddress = new Uri(configuration["RedditApi:ApiBaseAddress"]);
+            })
+            .AddClientAccessTokenHandler("reddit-api");
+
+        return services;
+    }
+}
