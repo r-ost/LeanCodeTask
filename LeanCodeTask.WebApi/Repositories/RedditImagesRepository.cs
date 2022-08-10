546a791667d9c7ee2fd2900025a12d5f2a198f34
Jan Szablanowski <szablanowsj@gmail.com>
2022-08-10 23:13:26 +0200

    Add RedditImagesRepository

---
+++ LeanCodeTask.WebApi/Repositories/RedditImagesRepository.cs
@@ -0,0 +1,48 @@
+using LeanCodeTask.WebApi.Database;
+using LeanCodeTask.WebApi.Database.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace LeanCodeTask.WebApi.Repositories;
+
+public interface IRedditImagesRepository
+{
+    Task<List<RedditImage>> GetAsync();
+    Task<RedditImage?> GetAsync(string id);
+    Task CreateAsync(RedditImage newImage);
+    Task UpdateAsync(string id, RedditImage updatedImage);
+    Task RemoveAsync(string id);
+}
+
+public class RedditImagesRepository : IRedditImagesRepository
+{
+    private readonly IMongoCollection<RedditImage> _imagesCollection;
+    
+    public RedditImagesRepository(IOptions<RedditImagesDatabaseSettings> redditImagesDatabaseSettings)
+    {
+        var mongoClient = new MongoClient(
+            redditImagesDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(
+            redditImagesDatabaseSettings.Value.DatabaseName);
+
+        _imagesCollection = mongoDatabase.GetCollection<RedditImage>(
+            redditImagesDatabaseSettings.Value.ImagesCollectionName);
+    }
+    
+    public async Task<List<RedditImage>> GetAsync() =>
+        await _imagesCollection.Find(_ => true).ToListAsync();
+
+    public async Task<RedditImage?> GetAsync(string id) =>
+        await _imagesCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+    public async Task CreateAsync(RedditImage newImage) =>
+        await _imagesCollection.InsertOneAsync(newImage);
+
+    public async Task UpdateAsync(string id, RedditImage updatedImage) =>
+        await _imagesCollection.ReplaceOneAsync(x => x.Id == id, updatedImage);
+
+    public async Task RemoveAsync(string id) =>
+        await _imagesCollection.DeleteOneAsync(x => x.Id == id);
+
+}
