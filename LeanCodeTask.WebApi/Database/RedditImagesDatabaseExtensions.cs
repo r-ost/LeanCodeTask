890f152fc915cedb1bb882ef28f92383e26f214a
Jan Szablanowski <szablanowsj@gmail.com>
2022-08-10 23:13:55 +0200

    Add RedditImagesDatabaseExtensions

---
+++ LeanCodeTask.WebApi/Database/RedditImagesDatabaseExtensions.cs
@@ -0,0 +1,16 @@
+using LeanCodeTask.WebApi.Repositories;
+using LeanCodeTask.WebApi.Utils;
+
+namespace LeanCodeTask.WebApi.Database;
+
+public static class RedditImagesDatabaseExtensions
+{
+    public static IServiceCollection AddRedditImagesDatabase(this IServiceCollection services, IConfiguration configuration)
+    { 
+        services.Configure<RedditImagesDatabaseSettings>(configuration.GetSection("RedditImagesDatabase"));
+        
+        services.AddSingleton<IRedditImagesRepository, RedditImagesRepository>();
+        
+        return services;
+    }
+}
